fc2342cb089f3dcda6c0a27b33c9c02031364c98
SamUltron <samurization@gmail.com>
2025-03-28 17:36:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// Iine color code 
+// CBCBCB
+
+// Text color to code
+// 4E2A0F 
+
+// Button color code
+//  CBA135 and F5F5F5
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
