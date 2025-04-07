ba40972fbd2666f42ff1b9887f69229b3aa749f3
SamUltron <samurization@gmail.com>
2025-04-07 22:56:52 +0100

    update images

--- src/data/detailss.js
+++ src/data/detailss.js
@@ -1,55 +1,57 @@
+import img1 from "/src/assets/Lovely.jpeg";
+import img2 from "/src/assets/2 Bed.png";
+import img3 from "/src/assets/secondd.jpeg";
+import img4 from "/src/assets/House.jpeg";
+import img5 from "/src/assets/bedroom.jpeg";
+import img6 from "/src/assets/brand.jpeg";
+
 export const details = [
-    {
-      id: 1,
-      image: "../src/assets/Lovely.jpeg",
-      title: "Three Bedroom Flat",
-      description: "3 Bedroom, 4 Bath", 
-      price: "500,000/Year",
-      "button": "View Details"
-      },
-
-      {
-      id: 2,
-      image: "../src/assets/2 Bed.png",
-      title: "Two Bedroom Flat",
-      description: "2 Bedroom, 3 Bath", 
-      price: "350,000/Year",
-      button: "View Details"
-      },
-
-      {
-          id: 3,
-          image: "../src/assets/secondd.jpeg",
-          title: "A Room Self Contain",
-          description: "1 Bedroom, 1 Bath", 
-          price: "250,000/Year",
-          button: "View Detail"
-      },
-    {
-      id: 4,
-      image: "../src/assets/House.jpeg",
-      title: "Three Bedroom Flat",
-      description: "3 Bedroom, 4 Bath", 
-      price: "500,000/Year",
-      button: "View Details"
-      },
-
-      {
-      id: 5,
-      image: "../src/assets/bedroom.jpeg",
-      title: "Two Bedroom Flat",
-      description: "2 Bedroom, 3 Bath", 
-      price: "350,000/Year",
-      button: "View Details"
-      },
-
-      {
-          id: 6,
-          image: "../src/assets/brand.jpeg",
-          title: "A Room Self Contain",
-          description: "1 Bedroom, 1 Bath", 
-          price: "250,000/Year",
-          button: "View Detail"
-      }
-    
-  ]
+  {
+    id: 1,
+    image: img1,
+    title: "Three Bedroom Flat",
+    description: "3 Bedroom, 4 Bath",
+    price: "500,000/Year",
+    button: "View Details"
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "Two Bedroom Flat",
+    description: "2 Bedroom, 3 Bath",
+    price: "350,000/Year",
+    button: "View Details"
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "A Room Self Contain",
+    description: "1 Bedroom, 1 Bath",
+    price: "250,000/Year",
+    button: "View Detail"
+  },
+  {
+    id: 4,
+    image: img4,
+    title: "Three Bedroom Flat",
+    description: "3 Bedroom, 4 Bath",
+    price: "500,000/Year",
+    button: "View Details"
+  },
+  {
+    id: 5,
+    image: img5,
+    title: "Two Bedroom Flat",
+    description: "2 Bedroom, 3 Bath",
+    price: "350,000/Year",
+    button: "View Details"
+  },
+  {
+    id: 6,
+    image: img6,
+    title: "A Room Self Contain",
+    description: "1 Bedroom, 1 Bath",
+    price: "250,000/Year",
+    button: "View Detail"
+  }
+];
