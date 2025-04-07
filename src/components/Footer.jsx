003417850c5bc7ec59b410c77a6331f930c1392b
SamUltron <samurization@gmail.com>
2025-04-07 21:08:06 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="flex flex-wrap justify-between p-10">
         <div className="space-y-3 md:w-1/4">
           <h2 className="md:text-3xl text-2xl font-semibold">NoAgent</h2>
-          <p className="text-lg">
+          <p className="text-lg  hover:text-[#71717a]">
             Connecting renters directly <br />with landlords, ensuring a <br />transparent,
             cost-effective, <br /> and hassle-free rental <br />experience.
           </p>
@@ -15,32 +15,32 @@ const Footer = () => {
         <div className="space-y-3 md:w-1/4">
           <h2 className="md:text-3xl text-2xl font-semibold">Quick Links</h2>
           <ul className="text-lg space-y-1">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Properties</li>
-            <li>For LandLords</li>
-            <li>Contact</li>
+            <li className=" hover:text-[#71717a]">Home</li>
+            <li className=" hover:text-[#71717a]">About Us</li>
+            <li className=" hover:text-[#71717a]">Properties</li>
+            <li className=" hover:text-[#71717a]">For LandLords</li>
+            <li className=" hover:text-[#71717a]">Contact</li>
           </ul>
         </div>
 
         <div className="space-y-3 md:w-1/4">
           <h2 className="md:text-3xl text-2xl font-semibold">Resources</h2>
           <ul className="text-lg space-y-1">
-            <li>Rental</li>
-            <li>Tenant Rights</li>
-            <li>Landlord Resources</li>
-            <li>FAQs</li>
-            <li>Blog</li>
+            <li className=" hover:text-[#71717a]">Rental</li>
+            <li className=" hover:text-[#71717a]">Tenant Rights</li>
+            <li className=" hover:text-[#71717a]">Landlord Resources</li>
+            <li className=" hover:text-[#71717a]">FAQs</li>
+            <li className=" hover:text-[#71717a]">Blog</li>
           </ul>
         </div>
 
         <div className="space-y-3 md:w-1/4">
           <h2 className="md:text-3xl text-2xl font-semibold">Legal</h2>
           <ul className="text-lg space-y-1">
-            <li>Terms of Service</li>
-            <li>Privacy Policy</li>
-            <li>Cookie Policy</li>
-            <li>Disclaimer</li>
+            <li className=" hover:text-[#71717a]">Terms of Service</li>
+            <li className=" hover:text-[#71717a]">Privacy Policy</li>
+            <li className=" hover:text-[#71717a]">Cookie Policy</li>
+            <li className=" hover:text-[#71717a]">Disclaimer</li>
           </ul>
         </div>
       </div>
